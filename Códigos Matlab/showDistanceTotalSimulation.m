e7b441a60d2b5d3d668a804b2c3e676b6c7bea7c
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-09-08 19:55:45 +0200

    Changed showDistance

--- Códigos Matlab/showDistanceTotalSimulation.m
+++ Códigos Matlab/showDistanceTotalSimulation.m
@@ -22,14 +22,17 @@ function [] = showDistanceTotalSimulation(filename)
         distances = [distances C{3}];
         counter = counter + 1;
     end
-    plot(distances, '-d', 'MarkerSize', 10);
+    plot(distances, '-d', 'MarkerSize', 10, 'LineWidth', 2);
     hold on;
     for i=1:size(changeOfEpisode,2)
-        plot(changeOfEpisode(i), distances(changeOfEpisode(i)), 'rd', 'MarkerSize', 10);
+        plot(changeOfEpisode(i), distances(changeOfEpisode(i)), 'rd', 'MarkerSize', 10, 'LineWidth', 2);
     end
     xlabel('Step'); ylabel('Distance');
-    set(gca, 'FontSize', 40);
-    set(gca, 'YTick', 0:10:120);
+    xlim([0 2000]); ylim([0 120]);
+    set(gca, 'FontSize', 30);
+    set(gca, 'YTick', 0:10:120);    
+    set(gca, 'XTick', 0:200:2000);
+    legend('Distances between V_i and V_{i-1}', 'Start of episode')
     grid on;
     ax =  gca;
     ax.GridAlpha = 0.5;
